7131eb8cfbe5a71edb0a63d5cd83c01383d320e4
Ammad Aslam <ammad.aslam@devbox.co>
2020-07-23 11:55:03 +0500

    update views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,4 +14,6 @@ require("channels")
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// const imagePath = (name) => images(name, true
+
+import "bootstrap"
