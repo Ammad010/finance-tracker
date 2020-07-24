2e55b7491688cb60ff702f3317a978c8e87f8559
Ammad Aslam <ammad.aslam@devbox.co>
2020-07-24 16:41:49 +0500

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'search_stock', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
+
+
 end
